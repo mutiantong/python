59240cfab669a9a3023cae59e5b9936d6cce1bd5
DSH-ZHURUN-WS\aa <samzhu92@gmail.com>
2018-07-12 16:59:24 +0800

    1. learn chapter 22 2. change the name of each file, because the "name" of the file is error.

---
+++ _1_syntax_practice/_1_learning_pyhont_version4/-22 chapter model 2/_22_1_model_1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     22-1-model-1
+   Description :
+   Author :      'Sam Yong'
+   date：          2018/7/12
+-------------------------------------------------
+   Change Activity:
+                   2018/7/12:
+-------------------------------------------------
+"""
+__author__ = 'Sam Yong'
+
+import sys
+
+
+def func():
+    pass
+
+
+class kclass:
+    pass
+
+
+name = 42
---
+++ _1_syntax_practice/_1_learning_pyhont_version4/-22 chapter model 2/_22_2_model_1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     22-2-impo-model-1
+   Description :
+   Author :      'Sam Yong'
+   date：          2018/7/12
+-------------------------------------------------
+   Change Activity:
+                   2018/7/12:
+-------------------------------------------------
+"""
+__author__ = 'Sam Yong'
+
+import _22_1_model_1
+print(list(( _22_1_model_1.__dict__.keys())))
