199d119433452d0adb8782759ffceab54fbd3e90
DSH-ZHURUN-WS\aa <samzhu92@gmail.com>
2018-07-13 11:34:30 +0800

    1. chapter 24 learning 2. add the sys.path.append

--- _1_syntax_practice/_1_learning_pyhont_version4/-23 chapter model package/dir1/__init__.py
+++ _1_syntax_practice/_1_learning_pyhont_version4/-23 chapter model package/dir1/__init__.py
@@ -1,1 +1,8 @@
+import os
+
+# Ensure compatibility issues are covered with pythondsn
+os.environ['EVENTLET_NO_GREENDNS'] = 'yes'
+
+# Make sure eventlet is loaded
+import eventlet  # noqa
 print('dir1 init')
---
+++ _1_syntax_practice/_1_learning_pyhont_version4/-24 chapter modern model/dir1/dir2/dir3/dir3haha.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     dir3haha
+   Description :
+   Author :      'Sam Yong'
+   date：          2018/7/13
+-------------------------------------------------
+   Change Activity:
+                   2018/7/13:
+-------------------------------------------------
+"""
+__author__ = 'Sam Yong'
+import os
+os.sys.path.append(r'D:\0_mutiantong_python\_1_syntax_practice\_1_learning_pyhont_version4\-24 chapter modern model')
+from dir1.haha import hahax
+
+#import dir1.haha.hahax as hhaaaa
+print(hahax)
+
+print(os.environ['PYTHONPATH'])
